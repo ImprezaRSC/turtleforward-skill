faed36d0ba000d5b41f8e6689de6dc3b5e7dffa6
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-02-14 20:54:58 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import subprocess
 
 class Turtleforward(MycroftSkill):
     def __init__(self):
@@ -8,7 +8,8 @@ class Turtleforward(MycroftSkill):
     @intent_file_handler('turtleforward.intent')
     def handle_turtleforward(self, message):
         self.speak_dialog('turtleforward')
-
+        s = "rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[2.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'"
+        subprocess.call([s],shell=True)
 
 def create_skill():
     return Turtleforward()
